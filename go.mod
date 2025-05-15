d90caf04d8851f36956c8022b179aa13e4b30d2e
ogneyar <maksimus.s@mail.ru>
2025-05-15 18:24:01 +0300

    ррр

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module idm
 
 go 1.24.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- inner/common/database.go
+++ inner/common/database.go
@@ -1,31 +1,24 @@
 package common
 
 import (
-	"idm/inner/common"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-// Временная переменная, которая будет ссылаться на подключение к базе данных. Позже мы от неё избавимся
-var DB *sqlx.DB
-
-// ConnectDb получить конфиг и подключиться с ним к базе данных
+// ConnectDb подключиться к базе данных и вернуть подключение
 func ConnectDb() *sqlx.DB {
-	cfg := GetConfig("docker/.env")
+	cfg := GetConfig(".env")
 	return ConnectDbWithCfg(cfg)
 }
 
-// ConnectDbWithCfg подключиться к базе данных с переданным конфигом
-func ConnectDbWithCfg(cfg common.Config) *sqlx.DB {
-	DB = sqlx.MustConnect(cfg.DbDriverName, cfg.Dsn)
-	// Настройки ниже конфигурируют пулл подключений к базе данных. Их названия стандартны для большинства библиотек.
-	// Ознакомиться с их описанием можно на примере документации Hikari pool:
-	// https://github.com/brettwooldridge/HikariCP?tab=readme-ov-file#gear-configuration-knobs-baby
-	DB.SetMaxIdleConns(5)
-	DB.SetMaxOpenConns(20)
-	DB.SetConnMaxLifetime(1 * time.Minute)
-	DB.SetConnMaxIdleTime(10 * time.Minute)
-	return DB
+// ConnectDbWithCfg подключиться к базе данных с использованием конфигурации и вернуть подключение
+func ConnectDbWithCfg(cfg Config) *sqlx.DB {
+	var db = sqlx.MustConnect(cfg.DbDriverName, cfg.Dsn)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(1 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+	return db
 }
